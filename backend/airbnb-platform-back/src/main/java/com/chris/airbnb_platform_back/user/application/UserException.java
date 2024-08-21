41c73caddba856bbad654b4b824985c50c21a710
chrispap1441 <papadopoulos.chris@hotmail.com>
2024-08-22 00:18:22 +0300

    Authentication and Authorization (Auth0) Backend

---
+++ backend/airbnb-platform-back/src/main/java/com/chris/airbnb_platform_back/user/application/UserException.java
@@ -0,0 +1,8 @@
+package com.chris.airbnb_platform_back.user.application;
+
+public class UserException extends RuntimeException{
+
+    public UserException(String message) {
+        super(message);
+    }
+}
--- backend/airbnb-platform-back/src/main/java/com/chris/airbnb_platform_back/user/mapper/UserMapper.java
+++ backend/airbnb-platform-back/src/main/java/com/chris/airbnb_platform_back/user/mapper/UserMapper.java
@@ -1,7 +1,16 @@
 package com.chris.airbnb_platform_back.user.mapper;
 
+import com.chris.airbnb_platform_back.user.application.dto.ReadUserDTO;
+import com.chris.airbnb_platform_back.user.domain.Authority;
+import com.chris.airbnb_platform_back.user.domain.User;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
-public class UserMapper {
+public interface UserMapper {
+
+    ReadUserDTO readUserDTOToUser(User user);
+
+    default String mapAuthoritiesToString(Authority authority) {
+        return authority.getName();
+    }
 }
